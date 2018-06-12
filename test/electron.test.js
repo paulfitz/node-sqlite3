de2475e7ee6d0c00290504c1e93ebb887452ac3e
Janet Vorobyeva <janet.vorobyeva@gmail.com>
2018-06-12 16:56:15 -0400

    Fixed testing on linux, readme, electron test (#2)
    * Fixed testing on linux, readme, electron test
    
    marshal.node failed to link on linux for marshal-test.js
    specified library in test/cpp/binding.gyp
    
    Electron test only worked if repo was checked out as exactly node-sqlite3
    also, module was sometimes cached, breaking the test (needed to delete it to force reload)

--- test/electron.test.js
+++ test/electron.test.js
@@ -3,7 +3,11 @@ var assert = require('assert');
 describe('electron', function() {
   it('respects ELECTRON_VERSION', function() {
     process.env.ELECTRON_VERSION = '1.2.3';
+    let name = require.resolve('..');
+    delete require.cache[name];
+
     assert.throws(function() { require('..'); },
-                  (/Cannot find module .*\/node-sqlite3\/lib\/binding\/electron-v1.2-[^-]+-x64\/node_sqlite3.node/));
+                  (/Cannot find module .*\/lib\/binding\/electron-v1.2-[^-]+-x64\/node_sqlite3.node/),
+                  "Should have error like 'cannot find module'");
   });
 });
